8da6b275ef18599428de1a01129d5ad890713f50
mohammadekhosravi <mohammadekhosravi@yahoo.com>
2024-12-08 20:26:01 +0330

    feat: format on save

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,2 @@
 require("config.lazy")
 require("options")
-
--- format go files when save a file
-vim.api.nvim_create_autocmd("BufWritePost", {
-  group = vim.api.nvim_create_augroup("formatGoCode", { clear = true }),
-  pattern = "*.go",
-  command = "silent exec '!gofmt -w %'",
-})
---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,43 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    }
+  },
+  config = function()
+    require('lspconfig').lua_ls.setup {}
+    require('lspconfig').gopls.setup {}
+
+    vim.api.nvim_create_autocmd('LspAttach', {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        -- check to see if client exist
+        if not client then return end
+
+        -- define formatting on save only if client supports formatting
+        if client.supports_method('textDocument/formatting') then
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            buffer = args.buf,
+            callback = function()
+              vim.lsp.buf.format {
+                -- Never request typescript-language-server for formatting
+                filter = function(c) return c.name ~= "tsserver" end,
+                bufnr = args.buf,
+                id = client.id,
+              }
+            end
+          })
+        end
+      end,
+    })
+  end
+}
