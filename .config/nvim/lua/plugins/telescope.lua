999fceacf270869d0f37a730d6ae73e159ecb2d6
mohammadekhosravi <mohammadekhosravi@yahoo.com>
2024-12-10 23:08:02 +0330

    feat: telescope setup

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,33 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
+    }
+  },
+  config = function()
+    require('telescope').setup {
+      pickers = {
+        find_files = {
+          theme = 'ivy',
+        },
+        live_grep = {
+          theme = 'ivy',
+        },
+      },
+      extensions = {
+        fzf = {},
+      }
+    }
+
+    -- To get fzf loaded and working with telescope, you need to call
+    -- load_extension, somewhere after setup function:
+    require('telescope').load_extension('fzf')
+  end,
+}
