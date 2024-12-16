e95389202b112cfcb4a60b9fb03309a2bb0858b7
mohammadekhosravi <mohammadekhosravi@yahoo.com>
2024-12-16 21:13:02 +0330

    feat: add keymaps for navigating quickfix list

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -4,3 +4,7 @@ local keymap = vim.keymap.set
 keymap('n', '<leader>fd', '<cmd>Telescope find_files<cr>', { desc = 'Fuzzy search files' })
 keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { desc = 'Fuzzy search neovim help guides' })
 keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = 'Fuzzy search neovim help guides' })
+
+-- Quickfix List
+keymap('n', '<M-j>', '<cmd>cnext<cr>', { desc = 'Go to next item in quickfix list' })
+keymap('n', '<M-k>', '<cmd>cprev<cr>', { desc = 'Go to previous item in quickfix list' })
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,25 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function() 
-    require'nvim-treesitter.configs'.setup {
+  config = function()
+    ---@diagnostic disable-next-line
+    require 'nvim-treesitter.configs'.setup {
       ensure_installed = { "bash", "comment", "cpp", "css", "csv", "diff", "dockerfile", "gitcommit", "go", "goctl", "gomod", "gosum", "gotmpl", "gowork", "html", "htmldjango", "javascript", "jq", "jsdoc", "json", "json5", "make", "python", "typescript", "scss", "sql", "styled", "tsx", "xml", "xresources", "yaml", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
       auto_install = false,
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+      -- List of parsers to ignore installing (or "all")
+      ignore_install = {},
       highlight = {
         enable = true,
         -- disable = { "c", "rust" },
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
+        disable = function(_, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
         end,
         additional_vim_regex_highlighting = false,
       },
